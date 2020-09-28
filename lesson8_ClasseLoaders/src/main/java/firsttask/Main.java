a724037ba71cf7c69004cdecf4f8ec140eef5858
OlegPetrov <antey1988@gmail.com>
2020-09-28 10:09:42 +0300

    lesson8_task1_forUbuntu

--- lesson8_ClasseLoaders/src/main/java/firsttask/Main.java
+++ lesson8_ClasseLoaders/src/main/java/firsttask/Main.java
@@ -5,11 +5,16 @@ public class Main {
 //        E:\User\Oleg\IdeaProjects\SberbankMaven\lesson8_ClasseLoaders\src\main\resources\RootDirectory\FirstPlugin
 //        E:\User\Oleg\IdeaProjects\SberbankMaven\lesson8_ClasseLoaders\src\main\resources\RootDirectory\SecondPlugin
         PluginManager pluginManager =
-                new PluginManager("E:\\User\\Oleg\\IdeaProjects\\SberbankMaven\\lesson8_ClasseLoaders\\src\\main\\resources\\RootDirectory");
+//                new PluginManager("E:\\User\\Oleg\\IdeaProjects\\SberbankMaven\\lesson8_ClasseLoaders\\src\\main\\resources\\RootDirectory");
+                new PluginManager("/home/oleg/IdeaProjects/SberbankMaven/lesson8_ClasseLoaders/src/main/resources/RootDirectory");
 
         Plugin p1 = new Plugin1_Imp();
         p1.doUsefull();
         System.out.println("Class " + p1.getClass().getClassLoader().toString());
+
+        Plugin p4 = new Plugin2_Imp();
+        p1.doUsefull();
+        System.out.println("Class " + p4.getClass().getClassLoader().toString());
 
         Plugin p2 = pluginManager.load("FirstPlugin", "Plugin1_Imp");
         p2.doUsefull();
