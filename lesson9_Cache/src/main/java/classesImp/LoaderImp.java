99e9e310048c4599dc33be5c5f567afab9531678
antey1988 <67778130+antey1988@users.noreply.github.com>
2020-09-29 20:43:55 +0300

    Update LoaderImp.java

--- lesson9_Cache/src/main/java/classesImp/LoaderImp.java
+++ lesson9_Cache/src/main/java/classesImp/LoaderImp.java
@@ -11,7 +11,6 @@ public class LoaderImp implements Loader {
     @Override
     public void doHardWork(String string, int n) {
         System.out.printf("Объект %s : выведена строка %s и число %d%n", this.toString(), string, n);
-
     }
 
     @Override
