9bebeb7de706c12c14782529598f53ca297ab777
OlegPetrov <antey1988@gmail.com>
2020-10-26 16:39:35 +0300

    lesson16 (after refactoring)

--- lesson16_Pattern/src/main/java/refactoring1/Tractor.java
+++ lesson16_Pattern/src/main/java/refactoring1/Tractor.java
@@ -3,34 +3,35 @@ package refactoring1;
 public class Tractor {
 
     int[] position = new int[] { 0, 0 };
-    int[] field = new int[] { 5, 5 };
+    final int[] field = new int[] { 5, 5 };
     Orientation orientation = Orientation.NORTH;
 
     public void move(String command) {
-        if (command == "F") {
+        if (command.equals("F")) {
             moveForwards();
-        } else if (command == "T") {
+        } else if (command.equals("T")) {
             turnClockwise();
         }
     }
 
     public void moveForwards() {
         if (orientation == Orientation.NORTH) {
-            position = new int[] { position[0], position[1] + 1 };
+            position[1]++;
         } else if (orientation == Orientation.EAST) {
-            position = new int[] { position[0] + 1, position[1] };
+            position[0]++;
         } else if (orientation == Orientation.SOUTH) {
-            position = new int[] { position[0], position[1] - 1 };
-        } else if (orientation == Orientation.WEST) {
-            position = new int[] { position[0] - 1, position[1] };
+            position[1]--;
+        } else {
+            position[0]--;
         }
+
         if (position[0] > field[0] || position[1] > field[1]) {
             throw new TractorInDitchException();
         }
     }
 
     public void turnClockwise() {
-        if (orientation == Orientation.NORTH) {
+        /*if (orientation == Orientation.NORTH) {
             orientation = Orientation.EAST;
         } else if (orientation == Orientation.EAST) {
             orientation = Orientation.SOUTH;
@@ -38,7 +39,11 @@ public class Tractor {
             orientation = Orientation.WEST;
         } else if (orientation == Orientation.WEST) {
             orientation = Orientation.NORTH;
-        }
+        }*/
+        int i = orientation.ordinal();
+        if (--i < 0)
+            i = 3;
+        orientation = Orientation.values()[i];
     }
 
     public int getPositionX() {
--- lesson16_Pattern/src/main/java/refactoring2/SalaryHtmlReportNotifier.java
+++ lesson16_Pattern/src/main/java/refactoring2/SalaryHtmlReportNotifier.java
@@ -22,47 +22,60 @@ public class SalaryHtmlReportNotifier {
 
     public void generateAndSendHtmlSalaryReport(String departmentId, LocalDate dateFrom, LocalDate dateTo, String recipients) {
         try {
-            // prepare statement with sql
-            PreparedStatement ps = connection.prepareStatement("select emp.id as emp_id, emp.name as amp_name, sum(salary) as salary from employee emp left join" +
-                    "salary_payments sp on emp.id = sp.employee_id where emp.department_id = ? and" +
-                    " sp.date >= ? and sp.date <= ? group by emp.id, emp.name");
-            // inject parameters to sql
-            ps.setString(0, departmentId);
-            ps.setDate(1, new java.sql.Date(dateFrom.toEpochDay()));
-            ps.setDate(2, new java.sql.Date(dateTo.toEpochDay()));
             // execute query and get the results
-            ResultSet results = ps.executeQuery();
+            ResultSet results = getSQLResult(departmentId, dateFrom, dateTo);
             // create a StringBuilder holding a resulting html
-            StringBuilder resultingHtml = new StringBuilder();
-            resultingHtml.append("<html><body><table><tr><td>Employee</td><td>Salary</td></tr>");
-            double totals = 0;
-            while (results.next()) {
-                // process each row of query results
-                resultingHtml.append("<tr>"); // add row start tag
-                resultingHtml.append("<td>").append(results.getString("emp_name")).append("</td>"); // appending employee name
-                resultingHtml.append("<td>").append(results.getDouble("salary")).append("</td>"); // appending employee salary for period
-                resultingHtml.append("</tr>"); // add row end tag
-                totals += results.getDouble("salary"); // add salary to totals
-            }
-            resultingHtml.append("<tr><td>Total</td><td>").append(totals).append("</td></tr>");
-            resultingHtml.append("</table></body></html>");
-            // now when the report is built we need to send it to the recipients list
-            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-            // we're going to use google mail to send this message
-            mailSender.setHost("mail.google.com");
-            // construct the message
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setTo(recipients);
-            // setting message text, last parameter 'true' says that it is HTML format
-            helper.setText(resultingHtml.toString(), true);
-            helper.setSubject("Monthly department salary report");
-            // send the message
-            mailSender.send(message);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (MessagingException e) {
+            StringBuilder resultingHtml = getHTMLTextFromSQL(results);
+            // send report to the recipients list
+            sendMessage(recipients, resultingHtml.toString());
+        } catch (SQLException | MessagingException e) {
             e.printStackTrace();
         }
+    }
+
+    private ResultSet getSQLResult(String departmentId, LocalDate dateFrom, LocalDate dateTo) throws SQLException {
+        // prepare statement with sql
+        PreparedStatement ps = connection.prepareStatement("select emp.id as emp_id, emp.name as amp_name, sum(salary) as salary from employee emp left join" +
+                "salary_payments sp on emp.id = sp.employee_id where emp.department_id = ? and" +
+                " sp.date >= ? and sp.date <= ? group by emp.id, emp.name");
+        // inject parameters to sql
+        ps.setString(0, departmentId);
+        ps.setDate(1, new java.sql.Date(dateFrom.toEpochDay()));
+        ps.setDate(2, new java.sql.Date(dateTo.toEpochDay()));
+        return ps.executeQuery();
+    }
+
+    private StringBuilder getHTMLTextFromSQL(ResultSet resultSet) throws SQLException {
+        // create a StringBuilder holding a resulting html
+        StringBuilder resultingHtml = new StringBuilder();
+        resultingHtml.append("<html><body><table><tr><td>Employee</td><td>Salary</td></tr>");
+        double totals = 0;
+        while (resultSet.next()) {
+            // process each row of query results
+            resultingHtml.append("<tr>"); // add row start tag
+            resultingHtml.append("<td>").append(resultSet.getString("emp_name")).append("</td>"); // appending employee name
+            resultingHtml.append("<td>").append(resultSet.getDouble("salary")).append("</td>"); // appending employee salary for period
+            resultingHtml.append("</tr>"); // add row end tag
+            totals += resultSet.getDouble("salary"); // add salary to totals
+        }
+        resultingHtml.append("<tr><td>Total</td><td>").append(totals).append("</td></tr>");
+        resultingHtml.append("</table></body></html>");
+        return resultingHtml;
+    }
+
+    private void sendMessage(String recipients, String htmlText) throws MessagingException {
+        // now when the report is built we need to send it to the recipients list
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        // we're going to use google mail to send this message
+        mailSender.setHost("mail.google.com");
+        // construct the message
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setTo(recipients);
+        // setting message text, last parameter 'true' says that it is HTML format
+        helper.setText(htmlText, true);
+        helper.setSubject("Monthly department salary report");
+        // send the message
+        mailSender.send(message);
     }
 }
